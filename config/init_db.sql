0aa3b172d04a5189b1b09bdead6a92a00adfa5b3
Staymix <staszubov199530@gmail.com>
2025-11-10 13:28:34 +0500

    HW_16

--- config/init_db.sql
+++ config/init_db.sql
@@ -9,6 +9,14 @@ CREATE TABLE contact (
   type        TEXT     NOT NULL,
   value       TEXT     NOT NULL
 );
-
 CREATE UNIQUE INDEX contact_uuid_type_index
   ON contact (resume_uuid, type);
+
+CREATE TABLE section (
+  id          SERIAL PRIMARY KEY,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  content     TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX section_idx
+  ON section (resume_uuid, type);
